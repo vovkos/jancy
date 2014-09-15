debfb2637decf02ad25ff4c39595cd7d4bcf56b7
Vladimir Gladkov <vovkos@gmail.com>
2014-09-15 13:55:10 +0800

    [git] remove cmake/latest/*.* from history

--- cmake/latest/jancy-x86.cmake
+++
@@ -1,9 +0,0 @@
-# This file is part of Jancy project
-# Tibbo Technology Inc (C) 2004-2014. All rights reserved
-# Author: Vladimir Gladkov
-
-#..............................................................................
-
-include (D:/Prj/Ninja/jancy/build/msvc10/cmake/jancy.cmake)
-
-#..............................................................................
--- cmake/latest/jancy.cmake
+++
@@ -1,9 +0,0 @@
-# This file is part of Jancy project
-# Tibbo Technology Inc (C) 2004-2014. All rights reserved
-# Author: Vladimir Gladkov
-
-#..............................................................................
-
-include ("${CMAKE_CURRENT_LIST_DIR}/jancy-${TARGET_CPU}.cmake")
-
-#..............................................................................
