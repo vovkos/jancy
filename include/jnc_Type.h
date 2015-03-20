76c1019fa2de6763e189bef23c10c4ee83d01143
Vladimir Gladkov <vovkos@gmail.com>
2015-03-20 15:38:46 +0800

    [jancy] remove jnc_VariantType.h

--- include/jnc_Type.h
+++ include/jnc_Type.h
@@ -750,5 +750,31 @@ getWeakPtrType (Type* type);
 
 //.............................................................................
 
+struct Variant
+{
+	Type* m_type;
+
+	union
+	{
+		int8_t m_int8;
+		uint8_t m_int8_u;
+		int16_t m_int16;
+		uint16_t m_int16_u;
+		int32_t m_int32;
+		uint32_t m_int32_u;
+		int64_t m_int64;
+		uint64_t m_int64_u;
+		float m_float;
+		double m_double;
+		void* m_p;
+		DataPtr m_dataPtr;
+		IfaceHdr* m_classPtr;
+		FunctionPtr m_functionPtr;
+		PropertyPtr m_propertyPtr;
+	};
+};
+
+//.............................................................................
+
 } // namespace jnc {
 
