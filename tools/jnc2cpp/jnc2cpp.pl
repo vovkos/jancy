f46a4cf5f4716db74e44c50e233c167e9a792e65
Vladimir Gladkov <vovkos@gmail.com>
2017-02-09 13:11:48 +0800

    [jnc2cpp] exclude C-comments from the generated source

--- tools/jnc2cpp/jnc2cpp.pl
+++ tools/jnc2cpp/jnc2cpp.pl
@@ -9,10 +9,27 @@
 #
 #...............................................................................
 
+# 0 -- idle
+# 1 -- converting to C-literal
+# 2 -- C-comment
+
 $state = 0;
 
 while (chomp (my $s = <>))
 {
+	if ($state == 2)
+	{
+		if ($s =~ m/\*\//)
+		{
+			$s = $';
+			$state = 1; # we only handle C-comments in state 1
+		}
+		else
+		{
+			next;
+		}
+	}
+	
 	if ($s =~ m/\/\/\//)
 	{
 		$body = $';
@@ -32,16 +49,31 @@ while (chomp (my $s = <>))
 			print ("$body\n");
 		}
 	}
-	elsif ($state)
+	elsif ($state == 1)
 	{
-		# get rid of single-line comments
-
-		$s =~ s/\/\/.*//;
-
+		# get rid of comments (don't bother handling multiple /**/ per line)
+	
+		if ($s =~ m/\/[\/*]/)
+		{
+			if ($& eq "/*")
+			{
+				$state = 2;
+				next;				
+			}
+			else
+			{
+				$s = $`;
+			}
+		}
+	
 		# if not an empty line or all-space line, make it C-literal
 
 		if ($s !~ m/^[ \t]*$/)
 		{
+			# remove trailing whitespace
+
+			$s =~ s/\s+$//;
+
 			# mask quotation marks with escapes
 
 			$s =~ s/(?<!\\)\"/\\\"/g;
