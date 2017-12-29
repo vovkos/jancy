ba6ad0828e9108ac38477b55f007678d219d6d97
Vladimir Gladkov <vovkos@gmail.com>
2017-12-29 15:42:15 +0800

    [jnc_api] move warning suppressions to a dedicated file (no pragma once)

--- include/jnc_Pch.h
+++ include/jnc_Pch.h
@@ -323,44 +323,6 @@ typedef wchar_t           utf32_t;
 
 //..............................................................................
 
-// warning suppression
-
-#if (_JNC_CPP_MSC)
-#	pragma warning (disable: 4146) // warning C4146: unary minus operator applied to unsigned type, result still unsigned
-#	pragma warning (disable: 4267) // warning C4267: 'var' : conversion from 'size_t' to 'type', possible loss of data
-#	pragma warning (disable: 4355) // warning C4355: 'this' : used in base member initializer list
-#endif
-
-#if (_JNC_CPP_GCC)
-
-#	pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-//#	pragma GCC diagnostic ignored "-Wint-to-pointer-cast"
-#	ifdef __cplusplus
-#		pragma GCC diagnostic ignored "-Winvalid-offsetof"
-#	endif
-//#	pragma GCC diagnostic ignored "-Wmissing-braces"
-#	pragma GCC diagnostic ignored "-Wmultichar"
-#	pragma GCC diagnostic ignored "-Wformat"
-#	if (__cplusplus >= 201103L)
-#		pragma GCC diagnostic ignored "-Wnarrowing"
-#	endif
-//#	pragma GCC diagnostic ignored "-Wparentheses"
-//#	pragma GCC diagnostic ignored "-Wsign-compare"
-//#	pragma GCC diagnostic ignored "-Wunused-parameter"
-//#	pragma GCC diagnostic ignored "-Wunused-result"
-//#	pragma GCC diagnostic ignored "-Wunused-variable"
-
-#endif
-
-#if (_JNC_CPP_CLANG)
-
-#	pragma GCC diagnostic ignored "-Wdangling-else"
-#	pragma GCC diagnostic ignored "-Wincompatible-ms-struct"
-#	pragma GCC diagnostic ignored "-Wlogical-op-parentheses"
-#	pragma GCC diagnostic ignored "-Wswitch"
-
-#endif
-
-//..............................................................................
-
 /// @}
+
+#include "jnc_WarningSuppression.h"
