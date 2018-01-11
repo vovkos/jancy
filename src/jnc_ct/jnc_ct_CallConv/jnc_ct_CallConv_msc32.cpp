0b4c59b92f5f665d4ff31c733c68959d27a2899f
Vladimir Gladkov <vovkos@gmail.com>
2018-01-11 17:58:43 +0800

    [jnc_ct] added a pragma-todo-warning re retval coercion on MSC 32-bit
    originally i thought i could rely on the default LLVM coercion for MSC-32, but apparently it doesn't alway work correctly (e.g. for struct with 3 ints LLVM will try to return it in eax:edx:ecx while MSVC will use byref retval)

--- src/jnc_ct/jnc_ct_CallConv/jnc_ct_CallConv_msc32.cpp
+++ src/jnc_ct/jnc_ct_CallConv/jnc_ct_CallConv_msc32.cpp
@@ -18,6 +18,12 @@ namespace ct {
 
 //..............................................................................
 
+#if (_JNC_CPP_MSC && JNC_PTR_SIZE == 4)
+
+AXL_TODO ("beware: structs with sizes between 16 and 24 are returned incorrectly (coercion must be implemented in XxxCallConv_msc32)")
+
+#endif
+
 //..............................................................................
 
 } // namespace ct
