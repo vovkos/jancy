47979609688b491779799b7f99d2ac50e0a02355
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2019-03-29 15:13:18 +0800

    [jnc_io] bugfix: Pcap doesn't use write buffer, but WriteBufferEmpty is now firing
    before, WriteBufferReady was not firing because zero-sized buffer is both empty and full (o_0)

--- src/jnc_ext/jnc_io_pcap/jnc_io_Pcap.h
+++ src/jnc_ext/jnc_io_pcap/jnc_io_Pcap.h
@@ -24,7 +24,7 @@ JNC_DECLARE_TYPE(PcapDeviceDesc)
 
 enum PcapEvent
 {
-	PcapEvent_Eof = 0x0100,
+	PcapEvent_Eof = 0x010,
 };
 
 //..............................................................................
