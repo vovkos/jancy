fa693e4f7d30761502e5bb171da919c17a80f68e
Vladimir Gladkov (laptop-arch) <vovkos@gmail.com>
2019-04-22 10:20:53 +0800

    [doc] moved doxyrest defines into doxyrest-config.lua files

---
+++ doc/api/doxyrest-config.lua
@@ -0,0 +1,18 @@
+--------------------------------------------------------------------------------
+--
+--  This file is part of the Jancy toolkit.
+--
+--  Jancy is distributed under the MIT license.
+--  For details see accompanying license.txt file,
+--  the public copy of which is also available at:
+--  http://tibbo.com/downloads/archive/jancy/license.txt
+--
+--------------------------------------------------------------------------------
+
+LANGUAGE = "cpp"
+ML_PARAM_LIST_COUNT_THRESHOLD = 1
+ML_SPECIFIER_MODIFIER_LIST = true
+EXCLUDE_DEFINE_PATTERN = "__STDC__"
+EXCLUDE_EMPTY_DEFINES = true
+EXCLUDE_PRIMITIVE_TYPEDEFS = true
+INTRO_FILE = "intro.rst"
---
+++ doc/stdlib/doxyrest-config.lua
@@ -0,0 +1,22 @@
+--------------------------------------------------------------------------------
+--
+--  This file is part of the Jancy toolkit.
+--
+--  Jancy is distributed under the MIT license.
+--  For details see accompanying license.txt file,
+--  the public copy of which is also available at:
+--  http://tibbo.com/downloads/archive/jancy/license.txt
+--
+--------------------------------------------------------------------------------
+
+INTRO_FILE = "intro.rst"
+LANGUAGE = "jancy"
+ML_PARAM_LIST_COUNT_THRESHOLD = 1
+FORCE_INCLUDE_FILE = "def.rst"
+
+EXTRA_PAGE_LIST =
+{
+	"syncId.rst",
+	"disposable.rst",
+	"errorcode.rst"
+}
