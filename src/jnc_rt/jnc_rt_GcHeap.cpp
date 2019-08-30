85b07d287c907f8b77b2872f6bb94a24730d840c
Vladimir Gladkov <vovkos@gmail.com>
2019-08-30 13:47:47 +0300

    [jnc_rt] opt: GcHeap::collect_l should use StdType_AbstractClassPtr instead of precise class pointer
    this way we likely reduce the overall number of types in the module

--- src/jnc_rt/jnc_rt_GcHeap.cpp
+++ src/jnc_rt/jnc_rt_GcHeap.cpp
@@ -1622,11 +1622,10 @@ GcHeap::collect_l(bool isMutatorThread)
 	{
 		size_t count = m_dynamicDestructArray.getCount();
 		IfaceHdr** iface = m_dynamicDestructArray;
+		Type* ptrType = m_runtime->getModule()->m_typeMgr.getStdType(StdType_AbstractClassPtr);
+
 		for (size_t i = 0; i < count; i++, iface++)
-		{
-			ct::ClassType* classType = (ct::ClassType*)(*iface)->m_box->m_type;
-			addRoot(iface, classType->getClassPtrType());
-		}
+			addRoot(iface, ptrType);
 
 		runMarkCycle();
 	}
