fdeb8d8b3cbba4a200500983731ec49a046062b6
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2019-10-23 13:43:36 +0800

    [tools] added file2literal.pl (will eventually replace jnc2cpp.pl)

---
+++ tools/file2literal/file2literal.pl
@@ -0,0 +1,17 @@
+#...............................................................................
+#
+#  This file is part of the Jancy toolkit.
+#
+#  Jancy is distributed under the MIT license.
+#  For details see accompanying license.txt file,
+#  the public copy of which is also available at:
+#  http://tibbo.com/downloads/archive/jancy/license.txt
+#
+#...............................................................................
+
+while (chomp(my $s = <>))
+{
+	$s =~ s/\\/\\\\/g;
+	$s =~ s/"/\\"/g;
+	print("\"$s\\n\"\n");
+}
