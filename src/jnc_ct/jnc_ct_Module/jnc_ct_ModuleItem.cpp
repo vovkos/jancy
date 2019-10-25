c4894e931c5869d8c9abf97ed700fedfab16437d
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2019-10-25 15:11:03 +0800

    [jnc_ct] use table-based lookups for ModuleItem::getDecl, ModuleItem::getNamespace, ModuleItem::getType

--- src/jnc_ct/jnc_ct_Module/jnc_ct_ModuleItem.cpp
+++ src/jnc_ct/jnc_ct_Module/jnc_ct_ModuleItem.cpp
@@ -98,6 +98,252 @@ ModuleItemBodyDecl::canSetBody()
 
 //..............................................................................
 
+ModuleItemDecl*
+getNullDecl(ModuleItem* item)
+{
+	return NULL;
+}
+
+ModuleItemDecl*
+getNamespaceDecl(ModuleItem* item)
+{
+	return (GlobalNamespace*)item;
+}
+
+ModuleItemDecl*
+getScopeDecl(ModuleItem* item)
+{
+	return (Scope*)item;
+}
+
+ModuleItemDecl*
+getAttributeDecl(ModuleItem* item)
+{
+	return (Attribute*)item;
+}
+
+ModuleItemDecl*
+getAttributeBlockDecl(ModuleItem* item)
+{
+	return (AttributeBlock*)item;
+}
+
+ModuleItemDecl*
+getTypeDecl(ModuleItem* item)
+{
+	return (((Type*)item)->getTypeKindFlags() & TypeKindFlag_Named) ? (NamedType*)item : NULL;
+}
+
+ModuleItemDecl*
+getTypedefDecl(ModuleItem* item)
+{
+	return (Typedef*)item;
+}
+
+ModuleItemDecl*
+getAliasDecl(ModuleItem* item)
+{
+	return (Alias*)item;
+}
+
+ModuleItemDecl*
+getConstDecl(ModuleItem* item)
+{
+	return (Const*)item;
+}
+
+ModuleItemDecl*
+getVariableDecl(ModuleItem* item)
+{
+	return (Variable*)item;
+}
+
+ModuleItemDecl*
+getFunctionDecl(ModuleItem* item)
+{
+	return (Function*)item;
+}
+
+ModuleItemDecl*
+getFunctionArgDecl(ModuleItem* item)
+{
+	return (FunctionArg*)item;
+}
+
+ModuleItemDecl*
+getFunctionOverloadDecl(ModuleItem* item)
+{
+	return (FunctionOverload*)item;
+}
+
+ModuleItemDecl*
+getPropertyDecl(ModuleItem* item)
+{
+	return (Property*)item;
+}
+
+ModuleItemDecl*
+getPropertyTemplateDecl(ModuleItem* item)
+{
+	return (PropertyTemplate*)item;
+}
+
+ModuleItemDecl*
+getEnumConstDecl(ModuleItem* item)
+{
+	return (EnumConst*)item;
+}
+
+ModuleItemDecl*
+getFieldDecl(ModuleItem* item)
+{
+	return (Field*)item;
+}
+
+ModuleItemDecl*
+getBaseTypeSlotDecl(ModuleItem* item)
+{
+	return (BaseTypeSlot*)item;
+}
+
+ModuleItemDecl*
+getOrphanDecl(ModuleItem* item)
+{
+	return (Orphan*)item;
+}
+
+// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+Namespace*
+getNullNamespace(ModuleItem* item)
+{
+	return NULL;
+}
+
+Namespace*
+getNamespaceNamespace(ModuleItem* item)
+{
+	return (GlobalNamespace*)item;
+}
+
+Namespace*
+getScopeNamespace(ModuleItem* item)
+{
+	return (Scope*)item;
+}
+
+Namespace*
+getTypeNamespace(ModuleItem* item)
+{
+	return (((Type*)item)->getTypeKindFlags() & TypeKindFlag_Named) ? (NamedType*)item : NULL;
+}
+
+Namespace*
+getTypedefNamespace(ModuleItem* item)
+{
+	return getTypeNamespace(((Typedef*)item)->getType());
+}
+
+Namespace*
+getPropertyNamespace(ModuleItem* item)
+{
+	return (Property*)item;
+}
+
+Namespace*
+getPropertyTemplateNamespace(ModuleItem* item)
+{
+	return (PropertyTemplate*)item;
+}
+
+// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
+Type*
+getNullType(ModuleItem* item)
+{
+	return NULL;
+}
+
+Type*
+getTypeType(ModuleItem* item)
+{
+	return (Type*)item;
+}
+
+Type*
+getTypedefType(ModuleItem* item)
+{
+	return ((Typedef*)item)->getType();
+}
+
+Type*
+getAliasType(ModuleItem* item)
+{
+	return ((Alias*)item)->getTargetItem()->getType();
+}
+
+Type*
+getConstType(ModuleItem* item)
+{
+	return ((Const*)item)->getValue().getType();
+}
+
+Type*
+getVariableType(ModuleItem* item)
+{
+	return ((Variable*)item)->getType();
+}
+
+Type*
+getFunctionType(ModuleItem* item)
+{
+	return ((Function*)item)->getType();
+}
+
+Type*
+getFunctionArgType(ModuleItem* item)
+{
+	return ((FunctionArg*)item)->getType();
+}
+
+Type*
+getPropertyType(ModuleItem* item)
+{
+	return ((Property*)item)->getType();
+}
+
+Type*
+getPropertyTemplateType(ModuleItem* item)
+{
+	return ((PropertyTemplate*)item)->calcType();
+}
+
+Type*
+getEnumConstType(ModuleItem* item)
+{
+	return ((EnumConst*)item)->getParentEnumType();
+}
+
+Type*
+getFieldType(ModuleItem* item)
+{
+	return ((Field*)item)->getType();
+}
+
+Type*
+getBaseTypeSlotType(ModuleItem* item)
+{
+	return ((BaseTypeSlot*)item)->getType();
+}
+
+Type*
+getOrphanType(ModuleItem* item)
+{
+	return ((Orphan*)item)->getFunctionType();
+}
+
+// . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+
 ModuleItem::ModuleItem()
 {
 	m_module = NULL;
@@ -108,142 +354,106 @@ ModuleItem::ModuleItem()
 ModuleItemDecl*
 ModuleItem::getDecl()
 {
-	switch (m_itemKind)
+	typedef
+	ModuleItemDecl*
+	GetDeclFunc(ModuleItem* item);
+
+	GetDeclFunc* getDeclFuncTable[ModuleItemKind__Count] =
 	{
-	case ModuleItemKind_Namespace:
-		return (GlobalNamespace*)this;
+		getNullDecl,             // ModuleItemKind_Undefined = 0,
+		getNamespaceDecl,        // ModuleItemKind_Namespace,
+		getScopeDecl,            // ModuleItemKind_Scope,
+		getAttributeDecl,        // ModuleItemKind_Attribute,
+		getAttributeBlockDecl,   // ModuleItemKind_AttributeBlock,
+		getTypeDecl,             // ModuleItemKind_Type,
+		getTypedefDecl,          // ModuleItemKind_Typedef,
+		getAliasDecl,            // ModuleItemKind_Alias,
+		getConstDecl,            // ModuleItemKind_Const,
+		getVariableDecl,         // ModuleItemKind_Variable,
+		getFunctionDecl,         // ModuleItemKind_Function,
+		getFunctionArgDecl,      // ModuleItemKind_FunctionArg,
+		getFunctionOverloadDecl, // ModuleItemKind_FunctionOverload,
+		getPropertyDecl,         // ModuleItemKind_Property,
+		getPropertyTemplateDecl, // ModuleItemKind_PropertyTemplate,
+		getEnumConstDecl,        // ModuleItemKind_EnumConst,
+		getFieldDecl,            // ModuleItemKind_Field,
+		getBaseTypeSlotDecl,     // ModuleItemKind_BaseTypeSlot,
+		getOrphanDecl,           // ModuleItemKind_Orphan,
+		getNullDecl,             // ModuleItemKind_LazyImport,
+	};
 
-	case ModuleItemKind_Scope:
-		return (Scope*)this;
-
-	case ModuleItemKind_Type:
-		return (((Type*)this)->getTypeKindFlags() & TypeKindFlag_Named) ?
-			(NamedType*)this :
-			NULL;
-
-	case ModuleItemKind_Typedef:
-		return (Typedef*)this;
-
-	case ModuleItemKind_Alias:
-		return (Alias*)this;
-
-	case ModuleItemKind_Const:
-		return (Const*)this;
-
-	case ModuleItemKind_Variable:
-		return (Variable*)this;
-
-	case ModuleItemKind_FunctionArg:
-		return (FunctionArg*)this;
-
-	case ModuleItemKind_Function:
-		return (Function*)this;
-
-	case ModuleItemKind_FunctionOverload:
-		return (FunctionOverload*)this;
-
-	case ModuleItemKind_Property:
-		return (Property*)this;
-
-	case ModuleItemKind_PropertyTemplate:
-		return (PropertyTemplate*)this;
-
-	case ModuleItemKind_EnumConst:
-		return (EnumConst*)this;
-
-	case ModuleItemKind_Field:
-		return (Field*)this;
-
-	case ModuleItemKind_BaseTypeSlot:
-		return (BaseTypeSlot*)this;
-
-	case ModuleItemKind_Orphan:
-		return (Orphan*)this;
-
-	default:
-		return NULL;
-	}
+	ASSERT((size_t)m_itemKind);
+	return getDeclFuncTable[(size_t)m_itemKind](this);
 }
 
 Namespace*
 ModuleItem::getNamespace()
 {
-	switch (m_itemKind)
+	typedef
+	Namespace*
+	GetNamespaceFunc(ModuleItem* item);
+
+	GetNamespaceFunc* getNamespaceFuncTable[ModuleItemKind__Count] =
 	{
-	case ModuleItemKind_Namespace:
-		return (GlobalNamespace*)this;
+		getNullNamespace,             // ModuleItemKind_Undefined = 0,
+		getNamespaceNamespace,        // ModuleItemKind_Namespace,
+		getScopeNamespace,            // ModuleItemKind_Scope,
+		getNullNamespace,             // ModuleItemKind_Attribute,
+		getNullNamespace,             // ModuleItemKind_AttributeBlock,
+		getTypeNamespace,             // ModuleItemKind_Type,
+		getTypedefNamespace,          // ModuleItemKind_Typedef,
+		getNullNamespace,             // ModuleItemKind_Alias,
+		getNullNamespace,             // ModuleItemKind_Const,
+		getNullNamespace,             // ModuleItemKind_Variable,
+		getNullNamespace,             // ModuleItemKind_Function,
+		getNullNamespace,             // ModuleItemKind_FunctionArg,
+		getNullNamespace,             // ModuleItemKind_FunctionOverload,
+		getPropertyNamespace,         // ModuleItemKind_Property,
+		getPropertyTemplateNamespace, // ModuleItemKind_PropertyTemplate,
+		getNullNamespace,             // ModuleItemKind_EnumConst,
+		getNullNamespace,             // ModuleItemKind_Field,
+		getNullNamespace,             // ModuleItemKind_BaseTypeSlot,
+		getNullNamespace,             // ModuleItemKind_Orphan,
+		getNullNamespace,             // ModuleItemKind_LazyImport,
+	};
 
-	case ModuleItemKind_Scope:
-		return (Scope*)this;
-
-	case ModuleItemKind_Typedef:
-		return ((Typedef*)this)->getType()->getNamespace();
-
-	case ModuleItemKind_Type:
-		return (((Type*)this)->getTypeKindFlags() & TypeKindFlag_Named) ?
-			(NamedType*)this :
-			NULL;
-
-	case ModuleItemKind_Property:
-		return (Property*)this;
-
-	case ModuleItemKind_PropertyTemplate:
-		return (PropertyTemplate*)this;
-
-	default:
-		return NULL;
-	}
+	ASSERT((size_t)m_itemKind);
+	return getNamespaceFuncTable[(size_t)m_itemKind](this);
 }
 
 Type*
 ModuleItem::getType()
 {
-	using namespace jnc;
+	typedef
+	Type*
+	GetTypeFunc(ModuleItem* item);
 
-	switch (m_itemKind)
+	GetTypeFunc* getTypeFuncTable[ModuleItemKind__Count] =
 	{
-	case ModuleItemKind_Type:
-		return (ct::Type*)this;
+		getNullType,             // ModuleItemKind_Undefined = 0,
+		getNullType,             // ModuleItemKind_Namespace,
+		getNullType,             // ModuleItemKind_Scope,
+		getNullType,             // ModuleItemKind_Attribute,
+		getNullType,             // ModuleItemKind_AttributeBlock,
+		getTypeType,             // ModuleItemKind_Type,
+		getTypedefType,          // ModuleItemKind_Typedef,
+		getAliasType,            // ModuleItemKind_Alias,
+		getConstType,            // ModuleItemKind_Const,
+		getVariableType,         // ModuleItemKind_Variable,
+		getFunctionType,         // ModuleItemKind_Function,
+		getFunctionArgType,      // ModuleItemKind_FunctionArg,
+		getNullType,             // ModuleItemKind_FunctionOverload,
+		getPropertyType,         // ModuleItemKind_Property,
+		getPropertyTemplateType, // ModuleItemKind_PropertyTemplate,
+		getEnumConstType,        // ModuleItemKind_EnumConst,
+		getFieldType,            // ModuleItemKind_Field,
+		getBaseTypeSlotType,     // ModuleItemKind_BaseTypeSlot,
+		getOrphanType,           // ModuleItemKind_Orphan,
+		getNullType,             // ModuleItemKind_LazyImport,
+	};
 
-	case ModuleItemKind_Typedef:
-		return ((ct::Typedef*)this)->getType();
-
-	case ModuleItemKind_Const:
-		return ((ct::Const*)this)->getValue().getType();
-
-	case ModuleItemKind_Variable:
-		return ((ct::Variable*)this)->getType();
-
-	case ModuleItemKind_FunctionArg:
-		return ((ct::FunctionArg*)this)->getType();
-
-	case ModuleItemKind_Function:
-		return ((ct::Function*)this)->getType();
-
-	case ModuleItemKind_Property:
-		return ((ct::Property*)this)->getType();
-
-	case ModuleItemKind_PropertyTemplate:
-		return ((ct::PropertyTemplate*)this)->calcType();
-
-	case ModuleItemKind_EnumConst:
-		return ((ct::EnumConst*)this)->getParentEnumType();
-
-	case ModuleItemKind_Field:
-		return ((ct::Field*)this)->getType();
-
-	case ModuleItemKind_BaseTypeSlot:
-		return ((ct::BaseTypeSlot*)this)->getType();
-
-	case ModuleItemKind_Orphan:
-		return ((ct::Orphan*)this)->getFunctionType();
-
-	case ModuleItemKind_Alias:
-		return ((ct::Alias*)this)->getTargetItem()->getType();
-
-	default:
-		return NULL;
-	}
+	ASSERT((size_t)m_itemKind);
+	return getTypeFuncTable[(size_t)m_itemKind](this);
 }
 
 sl::String
