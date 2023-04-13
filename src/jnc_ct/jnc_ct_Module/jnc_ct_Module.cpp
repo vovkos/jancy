ffee7b3774e61d96ba2b55a8a479a3c28eff6625
Vladimir Gladkov (office-mac-mini-m1) <vovkos@gmail.com>
2023-04-13 15:52:05 +0800

    [jnc_ct] minor: only map bzero on darwin

--- src/jnc_ct/jnc_ct_Module/jnc_ct_Module.cpp
+++ src/jnc_ct/jnc_ct_Module/jnc_ct_Module.cpp
@@ -303,7 +303,7 @@ Module::createLlvmExecutionEngine() {
 		m_functionMap["memset"] = (void*)memset;
 		m_functionMap["memcpy"] = (void*)memcpy;
 		m_functionMap["memmove"] = (void*)memmove;
-#if (_JNC_OS_POSIX)
+#if (_JNC_OS_DARWIN)
 		m_functionMap["_bzero"] = (void*)bzero;
 		m_functionMap["___bzero"] = (void*)bzero;
 #endif
