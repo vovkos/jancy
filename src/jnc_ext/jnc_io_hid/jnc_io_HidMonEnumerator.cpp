f3027d8730aa34e82d3fd41fc19620f245e74d4b
Vladimir Gladkov (office-arch) <vovkos@gmail.com>
2023-10-09 19:20:44 +0800

    [jnc_io_hid] implement the HIDMON enumerator

--- src/jnc_ext/jnc_io_hid/jnc_io_HidMonEnumerator.cpp
+++ src/jnc_ext/jnc_io_hid/jnc_io_HidMonEnumerator.cpp
@@ -37,27 +37,18 @@ createHidMonDeviceDesc(
 ) {
 	DataPtr descPtr = createData<HidMonDeviceDesc>(runtime);
 	HidMonDeviceDesc* dstDesc = (HidMonDeviceDesc*)descPtr.m_p;
-	initUsbDeviceStrings(dstDesc, srcDesc);
-	dstDesc->m_vendorId = srcDesc->m_vendorId;
-	dstDesc->m_productId = srcDesc->m_productId;
-	dstDesc->m_address = srcDesc->m_address;
-	dstDesc->m_port = srcDesc->m_port;
-	dstDesc->m_class = srcDesc->m_class;
-	dstDesc->m_subClass = srcDesc->m_subClass;
-	dstDesc->m_speed = srcDesc->m_speed;
-	dstDesc->m_nextPtr = jnc::g_nullDataPtr;
+	initUsbMonDeviceDescBase(dstDesc, srcDesc);
 	dstDesc->m_hidDeviceNamePtr = strDup(srcDesc->m_hidDeviceName);
-	dstDesc->m_captureDeviceNamePtr = strDup(srcDesc->m_captureDeviceName);
-	dstDesc->m_captureDeviceId = srcDesc->m_captureDeviceId;
-	dstDesc->m_releaseNumber = srcDesc->m_releaseNumber;
-	dstDesc->m_vendorId = srcDesc->m_vendorId;
-	dstDesc->m_productId = srcDesc->m_productId;
+	dstDesc->m_reportDescriptorPtr = memDup(
+		srcDesc->m_reportDescriptor,
+		srcDesc->m_reportDescriptor.getCount()
+	);
+	dstDesc->m_reportDescriptorSize =  srcDesc->m_reportDescriptor.getCount();
 	dstDesc->m_usagePage = srcDesc->m_usagePage;
 	dstDesc->m_usage = srcDesc->m_usage;
+	dstDesc->m_releaseNumber = srcDesc->m_releaseNumber;
 	dstDesc->m_interfaceId = srcDesc->m_interfaceId;
 	dstDesc->m_endpointId = srcDesc->m_endpointId;
-	dstDesc->m_reportDescriptorSize = srcDesc->m_reportDescriptor.getCount();
-	dstDesc->m_reportDescriptorPtr = memDup(srcDesc->m_reportDescriptor, dstDesc->m_reportDescriptorSize);
 	return descPtr;
 }
 
--- src/jnc_ext/jnc_io_hid/jnc_io_HidMonEnumerator.h
+++ src/jnc_ext/jnc_io_hid/jnc_io_HidMonEnumerator.h
@@ -11,7 +11,7 @@
 
 #pragma once
 
-#include "jnc_io_UsbDeviceStrings.h"
+#include "jnc_io_UsbMonDeviceDescBase.h"
 
 namespace jnc {
 namespace io {
@@ -20,30 +20,19 @@ JNC_DECLARE_TYPE(HidMonDeviceDesc)
 
 //..............................................................................
 
-struct HidMonDeviceDesc: UsbDeviceStrings {
+struct HidMonDeviceDesc: UsbMonDeviceDescBase {
 	JNC_DECLARE_TYPE_STATIC_METHODS(HidMonDeviceDesc)
 
 	DataPtr m_nextPtr;
 	DataPtr m_hidDeviceNamePtr;
-	DataPtr m_captureDeviceNamePtr;
-	uint_t m_captureDeviceId;
+	DataPtr m_reportDescriptorPtr;
+	size_t m_reportDescriptorSize;
 
-	uint16_t m_vendorId;
-	uint16_t m_productId;
 	uint16_t m_usagePage;
 	uint16_t m_usage;
 	uint16_t m_releaseNumber;
-
-	uint8_t m_address;
-	uint8_t m_port;
-	uint8_t m_class;
-	uint8_t m_subClass;
-	uint8_t m_speed;
 	uint8_t m_interfaceId;
 	uint8_t m_endpointId;
-
-	DataPtr m_reportDescriptorPtr;
-	size_t m_reportDescriptorSize;
 };
 
 // . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
