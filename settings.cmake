c2a3fee016d24d379534e2e43023b5d75d17fd95
Vladimir Gladkov (office-mac-mini-m1) <vovkos@gmail.com>
2023-12-15 16:12:19 +0800

    [cmake] require c++11,14,17 depending on the llvm version

--- settings.cmake
+++ settings.cmake
@@ -15,8 +15,3 @@ if(GCC)
 	# LLVM is compiled without RTTI by default, so set default to "no-rtti"
 	axl_override_setting_once(GCC_FLAG_CPP_RTTI "-fno-rtti")
 endif()
-
-if(APPLE) 
-	# newer LLVMs require C++14
-	axl_override_setting_once(GCC_FLAG_CPP_STANDARD "-std=c++14")
-endif()
