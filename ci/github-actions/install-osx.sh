701bb730bcf2ccf23a4d0bd66dfd1c441ffa4155
Vladimir Gladkov (laptop2-arch) <vovkos@gmail.com>
2025-12-14 13:03:15 +0800

    [ci] use brew --prefix (macos amd64 & macos arm64 use different prefixes)

--- ci/github-actions/install-osx.sh
+++ ci/github-actions/install-osx.sh
@@ -17,11 +17,13 @@ brew install ragel
 brew install p7zip
 brew install ${LLVM_VERSION}
 
+BREW_PREFIX=$(brew --prefix)
+
 # openssl is already installed, but not linked
 
-echo "set (OPENSSL_INC_DIR /usr/local/opt/openssl/include)" >> paths.cmake
-echo "set (OPENSSL_LIB_DIR /usr/local/opt/openssl/lib)" >> paths.cmake
+echo "set(OPENSSL_INC_DIR $BREW_PREFIX/opt/openssl/include)" >> paths.cmake
+echo "set(OPENSSL_LIB_DIR $BREW_PREFIX/opt/openssl/lib)" >> paths.cmake
 
 # homebrew llvm installation is not linked
 
-echo "set (LLVM_CMAKE_DIR /usr/local/opt/${LLVM_VERSION}/lib/cmake/llvm)" >> paths.cmake
+echo "set(LLVM_CMAKE_DIR $BREW_PREFIX/opt/${LLVM_VERSION}/lib/cmake/llvm)" >> paths.cmake
